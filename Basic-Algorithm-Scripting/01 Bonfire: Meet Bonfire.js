b160dbeb4173071cdbfac63532af413e003cb71a
Ilya Mikhaylus <ilyamikhaylus@gmail.com>
2015-09-27 10:57:40 +0300

    editing

--- Basic-Algorithm-Scripting/01 Bonfire: Meet Bonfire.js
+++ Basic-Algorithm-Scripting/01 Bonfire: Meet Bonfire.js
@@ -1,10 +1,4 @@
 /*Bonfire: Meet Bonfire
-Difficulty: 
-Your goal is to fix the failing test.
-
-First, run all the tests by clicking "Run tests" or by pressing Control + Enter.
-
-The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next Bonfire.
 
 Make this function return true no matter what. */
 
