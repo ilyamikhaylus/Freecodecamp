44c1cde07b3d19ed35df02a0ef7c6b4bd44a324c
Ilya Mikhaylus <ilyamikhaylus@gmail.com>
2015-09-29 13:07:50 +0300

    Create 09 Bonfire: Boo who.js

---
+++ Intermediate-Algorithm-Scripting/09 Bonfire: Boo who.js
@@ -0,0 +1,13 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+
+Here are some helpful links:
+Boolean Objects
+*/
+
+function boo(bool) {
+  return (bool === true || bool === false)
+}
+
+boo(1);
