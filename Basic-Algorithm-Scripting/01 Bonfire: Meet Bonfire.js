e5a22e3b0d42913a3abf0a2eeb852639943249d6
Ilya Mikhaylus <ilyamikhaylus@gmail.com>
2015-09-29 11:43:35 +0300

    Update 01 Bonfire: Meet Bonfire.js

--- Basic-Algorithm-Scripting/01 Bonfire: Meet Bonfire.js
+++ Basic-Algorithm-Scripting/01 Bonfire: Meet Bonfire.js
@@ -1,14 +1,10 @@
-/*Bonfire: Meet Bonfire
-
-Make this function return true no matter what. */
+/*
+Make this function return true no matter what. 
+*/
 
 function meetBonfire(argument) {
-  // Good luck!
   console.log("you can read this function's argument in the developer tools", argument);
-
   return true;
 }
-
-
 
 meetBonfire("You can do this!");
