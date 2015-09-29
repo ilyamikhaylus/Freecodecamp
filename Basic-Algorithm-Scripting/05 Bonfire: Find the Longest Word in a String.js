24ea93a629cbed03937358366d04a013bc0ecd3a
Ilya Mikhaylus <ilyamikhaylus@gmail.com>
2015-09-29 11:44:52 +0300

    Update 05 Bonfire: Find the Longest Word in a String.js

--- Basic-Algorithm-Scripting/05 Bonfire: Find the Longest Word in a String.js
+++ Basic-Algorithm-Scripting/05 Bonfire: Find the Longest Word in a String.js
@@ -1,7 +1,7 @@
 /*
 Return the length of the longest word in the provided sentence.
 Your response should be a number.
-Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
 Here are some helpful links:
 String.split()
 String.length
